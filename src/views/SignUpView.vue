8534de5960459d9bfdb7e3dfa9d927723147f262
Sarfo Twumasi Bernard <bst@ddfinance.com>
2022-03-20 22:21:15 +0000

    Code Review

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -44,7 +44,6 @@
                 <input id="confirm-password" name="confirm-password" type="password" autocomplete="password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirm Password" />
               </div>
             </div>
-
             <div>
               <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 SIGN UP
